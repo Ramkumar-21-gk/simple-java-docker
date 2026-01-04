8c752a01bf2ffb907260d3ff8e0c025df9683479
Ramkumar-21-gk <gkramkumarprajapati49@gmail.com>
2026-01-04 15:07:57 +0530

    inCorrect print statement syntax in Main.java
    Fix syntax errors in print statements.

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Date currentDate = new Date();
         System.out.println("Hello, Docker! Current date: " + currentDate);
-        System.out.println("hey");
-        System.out.println("my name is:");
+        System.out.println"hey";
+        System.out.println my name is:");
         System.out.println("RamKumar v1");
             
     }
