6ddab091edefd302bb15f9bb6bea6840c555fa59
Ramkumar-21-gk <gkramkumarprajapati49@gmail.com>
2026-01-04 15:11:28 +0530

    Fix output messages in Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Date currentDate = new Date();
         System.out.println("Hello, Docker! Current date: " + currentDate);
-        System.out.println"hey";
-        System.out.println my name is:");
-        System.out.println("RamKumar v1");
+        System.out.println("hey");
+        System.out.println("my name is:");
+        System.out.println("RamKumar Prajapati v1");
             
     }
 }
